380db06c27e8c3244517c28b92c0ea6505397884
kristian1309 <158219931+kristian1309@users.noreply.github.com>
2024-01-31 22:27:35 +0100

    title edit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-number_1 = st.number_input('Insert a number_1')
+
+st.title('kristians calc')
+
+
+
+number_1 = st.number_input('Insert  number_1')
 number_2 = st.number_input('Insert a number_2')
 calc=number_1*number_2
 st.write('The current number is ', calc)
